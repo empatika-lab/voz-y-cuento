e823c130d11699674d88027efd036717bf7ea0f2
leanhanc <leandrohanc@gmail.com>
2024-10-15 09:22:58 -0300

    Add disabled styles to pending course

--- src/components/Course/CourseCard.tsx
+++ src/components/Course/CourseCard.tsx
@@ -6,8 +6,6 @@ import arrowRight from '@images/icons/arrow-right.svg';
 
 /* Types */
 import type { Course } from '@/payload/payload-types';
-
-/* Utils */
 import { cn } from '@/lib/utils/classNames';
 
 export interface CardCoursesProps {
@@ -15,13 +13,13 @@ export interface CardCoursesProps {
 	isAcademy?: boolean;
 }
 
-export default function CardCourse({ course, isAcademy = false }: CardCoursesProps) {
+export default function CourseCard({ course, isAcademy = false }: CardCoursesProps) {
 	return (
 		<li
 			key={course.id}
 			className={cn(
 				'flex w-[320px] flex-col rounded-2xl border border-solid border-gray-900 bg-white shadow-lg transition-transform ease-in-out will-change-transform hover:scale-[1.03] active:border-cyan-600 lg:w-[356px]',
-				course.isPending ? 'opacity-50' : 'opacity-100',
+				{ 'pointer-events-none': course.isPending },
 			)}
 		>
 			<NextLink href={isAcademy ? `/escuela/cursos/${course.slug}` : `/cursos/${course.slug}`}>
@@ -33,7 +31,16 @@ export default function CardCourse({ course, isAcademy = false }: CardCoursesPro
 							className="rounded-t-2xl object-cover"
 							fill
 							src={`${process.env.NEXT_PUBLIC_WEB_URL}${course.image.url!}`}
+							quality={66}
 						/>
+					)}
+
+					{course.isPending && (
+						<div className="absolute top-[50%] left-[50%] h-full w-full -translate-x-[50%] -translate-y-[50%] rounded-t-2xl z-10">
+							<span className="bg-yellow-200 p-4 font-bold text-black absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] w-full text-center uppercase">
+								Pago Pendiente
+							</span>
+						</div>
 					)}
 
 					{/* Category Badge */}
@@ -52,7 +59,7 @@ export default function CardCourse({ course, isAcademy = false }: CardCoursesPro
 				</div>
 
 				{/* Card Content */}
-				<div className="h-[156px] px-4 py-2 lg:h-[171px] lg:py-4">
+				<div className="h-[156px] px-4 py-2 lg:h-[171px] lg:py-4 relative">
 					<header className="max-w-[80%]">
 						<strong className="text-[20px]">{course.name}</strong>
 					</header>
@@ -61,14 +68,20 @@ export default function CardCourse({ course, isAcademy = false }: CardCoursesPro
 					)}
 				</div>
 
+				{course.isPending && (
+					<div className="absolute inset-0 h-full rounded-2xl w-full bg-black/60 p-4" aria-hidden />
+				)}
+
 				{/* Card Footer */}
-				<footer className="w-full px-4 py-2 lg:py-4">
-					<NextImage
-						alt="Ver detalle de Curso"
-						className="ml-auto h-[20px] w-[20px]"
-						src={arrowRight}
-					/>
-				</footer>
+				{!course.isPending && (
+					<footer className="w-full px-4 py-2 lg:py-4">
+						<NextImage
+							alt="Ver detalle de Curso"
+							className="ml-auto h-[20px] w-[20px]"
+							src={arrowRight}
+						/>
+					</footer>
+				)}
 			</NextLink>
 		</li>
 	);
