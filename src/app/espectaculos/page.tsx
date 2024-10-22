1e811c68a4697d11e4814fa4d654f73cac68222a
leanhanc <leandrohanc@gmail.com>
2024-10-22 10:52:10 -0300

    Try fixing cache

--- src/app/espectaculos/page.tsx
+++ src/app/espectaculos/page.tsx
@@ -2,7 +2,6 @@ import configPromise from '@payload-config';
 import { getPayloadHMR } from '@payloadcms/next/utilities';
 
 /* Utils */
-import { prettyPrint } from '@/lib/utils/dev';
 import { getCachedPayload } from '@/lib/utils/localApi';
 
 /* Components */
@@ -22,22 +21,13 @@ const breadcrumbItems = [
 	},
 ];
 
-async function fetchEvents() {
+export default async function HireMe() {
 	const payload = await getPayloadHMR({
 		config: configPromise,
 	});
 	const cachedPayload = getCachedPayload(payload);
 
-	const events = cachedPayload.find({ collection: 'events', limit: 1000 }).catch((e) => {
-		prettyPrint(e);
-	});
-
-	return events;
-}
-
-export default async function HireMe() {
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	const events = await fetchEvents();
+	const events = await cachedPayload.find({ collection: 'events' });
 
 	if (!events) {
 		return null;
