733808ea2e42638e104d6942a06fde45dd4ff5be
leanhanc <leandrohanc@gmail.com>
2024-10-24 15:53:56 -0300

    OnPremises update

--- src/app/(home)/OnPremises/OnPremises.tsx
+++ src/app/(home)/OnPremises/OnPremises.tsx
@@ -7,25 +7,30 @@ import NextImage from 'next/image';
 
 export default function OnPremises() {
 	return (
-		<section className="container py-24 pb-40">
-			<h2 className="font-display text-center text-4xl lg:text-7xl">
-				¡VozYcuento también sale de viaje!
-			</h2>
-			<div className="flex flex-col lg:flex-row mt-20 items-center gap-14">
-				<div className="flex flex-col lg:text-2xl text-pretty">
-					<p>
-						La Escuela de Narración Oral no sólo ofrece cursos virtuales, sino que también organiza
-						<strong> talleres presenciales</strong> coordinando con tu biblioteca, Institución
-						educativa u oraganización.
-					</p>
-					<p className="mt-14">Escribime para recibir más detalles.</p>
+		<section className="py-24 pb-40 bg-yellow-50">
+			<div className="container">
+				<h2 className="font-display text-center text-4xl lg:text-7xl">
+					¡VozYcuento también sale de viaje!
+				</h2>
+				<div className="flex flex-col lg:flex-row mt-20 items-center gap-14">
+					<div className="flex flex-col lg:text-2xl text-pretty">
+						<p>
+							La Escuela de Narración Oral no sólo ofrece cursos virtuales, sino que también
+							organiza
+							<strong> talleres presenciales</strong> coordinando con tu biblioteca, Institución
+							educativa u oraganización.
+						</p>
+						<p className="mt-14">Escribime para recibir más detalles.</p>
 
-					<Button className="py-16 mt-20 text-center w-fit" href="https://wa.me/5492215677747">
-						Contáctame
-					</Button>
+						<footer className="mt-20 mx-auto lg:mx-0">
+							<Button className="text-center lg:px-24 " href="https://wa.me/5492215677747">
+								Contáctame
+							</Button>
+						</footer>
+					</div>
+
+					<NextImage src={Suitcase} alt="Valija de viaje" className="mt-16 lg:mt-0" />
 				</div>
-
-				<NextImage src={Suitcase} alt="Valija de viaje" className="mt-16 lg:mt-0" />
 			</div>
 		</section>
 	);
