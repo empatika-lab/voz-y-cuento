97bc6793faf97d39f5aa540509e4e75a048a1809
leanhanc <leandrohanc@gmail.com>
2024-11-07 12:56:14 -0300

    Fix import

--- src/app/escuela/cursos/[slug]/comprar/components/BuyCourseCard.tsx
+++ src/app/escuela/cursos/[slug]/comprar/components/BuyCourseCard.tsx
@@ -1,7 +1,7 @@
 import NextImage from 'next/image';
 
 /* Types */
-import type { Course, Media } from '@/payload/payload-types';
+import type { Course, Media } from '@/payload-types';
 
 interface BuyCourseCardProps {
 	course: Course;
@@ -9,30 +9,30 @@ interface BuyCourseCardProps {
 
 export default function BuyCourseCard({ course }: BuyCourseCardProps) {
 	return (
-		<article className="flex flex-col lg:flex-row p-5 rounded-xl border-gray-400 border mt-12">
+		<article className="mt-12 flex flex-col rounded-xl border border-gray-400 p-5 lg:flex-row">
 			<div className="flex gap-8 lg:gap-0">
-				<div className="w-[122px] h-[122px] relative">
+				<div className="relative h-[122px] w-[122px]">
 					<NextImage
 						src={(course.image as Media).url!}
 						alt={course.name}
 						fill
-						className="rounded-lg shadow-2xl w-full h-full object-cover"
+						className="h-full w-full rounded-lg object-cover shadow-2xl"
 						priority
 					/>
 				</div>
 
-				<header className="flex-0 mt-3 ml-auto lg:ml-[50px]">
+				<header className="flex-0 ml-auto mt-3 lg:ml-[50px]">
 					<h1 className="flex flex-col">
-						<span className="lg:font-bold text-sm lg:text-lg">Curso</span>
-						<span className="font-bold text-sm lg:text-lg mt-3">{course.name}</span>
+						<span className="text-sm lg:text-lg lg:font-bold">Curso</span>
+						<span className="mt-3 text-sm font-bold lg:text-lg">{course.name}</span>
 					</h1>
 				</header>
 			</div>
 
-			<div className="flex-1 flex flex-col ml-auto text-right lg:justify-between text-[28px]">
-				<p className="font-bold text-pink-900 text-xl">ARS ${course.arsPrice}</p>
-				<p className="text-sm lg:flex lg:flex-col lg:items-end lg:justify-en">
-					<span className="font-bold ">USD ${course.usdPrice}</span>
+			<div className="ml-auto flex flex-1 flex-col text-right text-[28px] lg:justify-between">
+				<p className="text-xl font-bold text-pink-900">ARS ${course.arsPrice}</p>
+				<p className="lg:justify-en text-sm lg:flex lg:flex-col lg:items-end">
+					<span className="font-bold">USD ${course.usdPrice}</span>
 					<span className="text-grey-400 mt-3 lg:mt-0"> (fuera de Argentina)</span>
 				</p>
 			</div>
