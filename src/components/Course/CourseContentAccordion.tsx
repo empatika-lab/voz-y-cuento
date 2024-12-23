f1f252962c4dfe57a671f19790909ce40c53b1cc
leanhanc <leandrohanc@gmail.com>
2024-12-23 18:04:52 -0300

    CourseContentAccordion fixes and updates

--- src/components/Course/CourseContentAccordion.tsx
+++ src/components/Course/CourseContentAccordion.tsx
@@ -11,7 +11,8 @@ import plusIcon from '@images/icons/plus.svg';
 import minusIcon from '@images/icons/minus.svg';
 import videoIcon from '@images/icons/video.svg';
 import pencilIcon from '@images/icons/pencil.svg';
-
+import archiveIcon from '@images/icons/archive.svg';
+import presentationIcon from '@images/icons/presentation.svg';
 /* Types */
 import type { Course } from '@/payload-types';
 
@@ -142,16 +143,34 @@ function AccordionItemContent({ content }: AccordionItemContentProps) {
 			return <NextImage alt="Ejercicio" height={16} src={pencilIcon as string} width={16} />;
 		}
 
+		if (blockType === 'additional-material') {
+			return (
+				<NextImage alt="Material Adicional" height={16} src={archiveIcon as string} width={16} />
+			);
+		}
+
+		if (blockType === 'presentation') {
+			return (
+				<NextImage alt="Presentación" height={16} src={presentationIcon as string} width={16} />
+			);
+		}
+
 		return null;
 	}
 
 	return (
 		<ul className="bg-gray-50">
 			{content.map((item) => {
+				const title =
+					item.blockType === 'additional-material' ? 'Recursos Adicionales' : item.blockName;
+
 				return (
-					<li key={item.id} className="flex items-center rounded-lg px-2 py-4 lg:px-[152px]">
+					<li
+						key={item.id}
+						className="line-clamp-1 flex items-center truncate rounded-lg px-2 py-4 lg:px-[152px]"
+					>
 						{getIcon(item.blockType)}
-						{item.blockName && <p className="pl-4">{item.blockName}</p>}
+						{item.blockName && <p className="pl-4">{title}</p>}
 					</li>
 				);
 			})}
@@ -189,7 +208,7 @@ export default function CourseContentAccordion({ blocks }: CourseContentAccordio
 									</p>
 
 									{/* Desktop */}
-									<p className="hidden py-4 pr-2 font-bold lg:block">
+									<p className="hidden truncate py-4 pr-2 font-bold lg:block lg:max-w-[85%]">
 										<span className="pr-10">Bloque {index + 1}</span>
 										{block.name && <span className="pl-3 font-normal">{block.name}</span>}
 									</p>
