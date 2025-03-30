acbc665a67c49a38f06b5c05f049f9084ede7339
leanhanc <leandrohanc@gmail.com>
2025-03-30 16:47:04 -0300

    BuyCourseInstructions

--- src/app/(frontend)/escuela/cursos/[slug]/comprar/components/BuyCourseInstructions.tsx
+++ src/app/(frontend)/escuela/cursos/[slug]/comprar/components/BuyCourseInstructions.tsx
@@ -1,9 +1,11 @@
 export default function BuyCourseInstruction() {
 	return (
-		<article className="bg-yellow-100 p-4 font-bold mt-16">
-			<p>Una vez hecho el pago envía a tu comprobante a vozycuento@gmail.com.</p>
-			<p>Cuando lo hayamos recibido, te daremos acceso al curso.</p>
-			<p>Te llegará un correo electrónico para notificarte de que tenés acceso al mismo.</p>
+		<article className="mt-16 bg-yellow-100 p-4 font-bold">
+			<p>Una vez hecho el pago envia a tu comprobante a vozycuento@gmail.com.</p>
+			<p>
+				Una vez que hayamos verificado el pago te enviaremos un correo electrónico con el enlace
+				para que accedas al curso.
+			</p>
 		</article>
 	);
 }
